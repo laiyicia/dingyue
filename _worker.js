6eb8891ad98532f6c46ce6dcb43e68390c910c24
laiyicia <86539812+laiyicia@users.noreply.github.com>
2024-10-22 10:05:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,7 +12,7 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
+vless://1f5cfa92-4462-404b-aa3c-ddf3d36fca37@jp.fanjf001.sbs:20010?type=tcp&security=reality&sni=ip.sb&pbk=R2gKMF0Tetlnesc1pPkZH9NaOeehw-f5_U9JKG_cLjU&flow=xtls-rprx-vision&fp=chrome#JP|HYO3.love@xray.com
 https://sub.xf.free.hr/auto
 `
 
